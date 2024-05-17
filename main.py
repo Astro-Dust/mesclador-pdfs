7f13598a7f98797be2f49a19441472daac428f53
Astro-Dust <lucaslucenagdrive@gmail.com>
2024-05-16 23:22:22 -0300

    ordenando arquivos por data de adição e melhoria de codigo

--- main.py
+++ main.py
@@ -4,10 +4,13 @@ import os
 # criando um objeto
 merger = PyPDF2.PdfMerger()
 
+path_pdfs = 'pdfs_para_mesclar'
+
 lista_pdfs = os.listdir('pdfs_para_mesclar')
 
-for pdf in lista_pdfs:
-  if '.pdf' in pdf:
-    merger.append(f'pdfs_para_mesclar/{pdf}')
+# ordenando os arquivos por data (mais antiga para mais nova)
+lista_pdfs_ordenada = sorted(os.listdir(path_pdfs), key=lambda x: os.path.getmtime(os.path.join(path_pdfs, x)))
 
-merger.write('pdfs_mesclados.pdf')
+for pdf in lista_pdfs_ordenada:
+  if '.pdf' in pdf:
+    merger.append(f'{path_pdfs}/{pdf}')
